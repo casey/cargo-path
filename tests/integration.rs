fb351b693a4d91f6db981164e973c9a63ccc9889
Casey Rodarmor <casey@rodarmor.com>
2026-06-10 16:48:15 -0700

    Amend

--- tests/integration.rs
+++ tests/integration.rs
@@ -1,26 +1,49 @@
 use {
   regex::Regex,
-  std::{fs, process::Command},
+  std::{fs, path::Path, process::Command},
 };
 
-#[test]
-fn single_dependency() {
+const REGEX_VERSION: &str = "1.12.3";
+
+#[track_caller]
+fn case(dir: &Path, dependency: &str, found: bool) {
   let output = Command::new(env!("CARGO_BIN_EXE_cargo-path"))
-    .args(["path", "serde"])
+    .args(["path", dependency])
+    .current_dir(dir)
     .output()
     .unwrap();
 
-  assert!(output.status.success());
+  if found {
+    assert!(
+      output.status.success(),
+      "{}",
+      String::from_utf8_lossy(&output.stderr),
+    );
 
-  let stdout = str::from_utf8(&output.stdout).unwrap();
+    let stdout = str::from_utf8(&output.stdout).unwrap();
 
-  let regex =
-    Regex::new(r"^.*/\.cargo/registry/src/index\.crates\.io-[0-9a-f]*/serde-1\.0\.228\n$").unwrap();
+    let regex = Regex::new(&format!(
+      r"^.*/\.cargo/registry/src/index\.crates\.io-[0-9a-f]*/regex-{}\n$",
+      regex::escape(REGEX_VERSION),
+    ))
+    .unwrap();
 
-  assert!(
-    regex.is_match(stdout),
-    "regex mismatch: {stdout} ~= {regex}",
-  );
+    assert!(
+      regex.is_match(stdout),
+      "regex mismatch: {stdout} ~= {regex}",
+    );
+  } else {
+    assert!(!output.status.success());
+    assert_eq!(
+      str::from_utf8(&output.stderr).unwrap(),
+      format!("error: dependency `{dependency}` not found\n"),
+    );
+  }
+}
+
+#[test]
+fn single_dependency() {
+  case(Path::new("."), "regex", true);
 }
 
 #[test]
@@ -29,62 +52,25 @@ fn feature_gated_dependency() {
 
   fs::write(
     dir.path().join("Cargo.toml"),
-    "[package]
+    format!(
+      "[package]
 name = \"foo\"
 version = \"0.0.0\"
 edition = \"2024\"
 
 [dependencies]
-bar = { path = \"bar\", optional = true }
+regex = {{ version = \"={REGEX_VERSION}\", optional = true }}
 
 [features]
-baz = [\"dep:bar\"]
-",
+bar = [\"dep:regex\"]
+"
+    ),
   )
   .unwrap();
 
   fs::create_dir(dir.path().join("src")).unwrap();
   fs::write(dir.path().join("src/lib.rs"), "").unwrap();
 
-  fs::create_dir_all(dir.path().join("bar/src")).unwrap();
-  fs::write(
-    dir.path().join("bar/Cargo.toml"),
-    "[package]
-name = \"bar\"
-version = \"0.0.0\"
-edition = \"2024\"
-",
-  )
-  .unwrap();
-  fs::write(dir.path().join("bar/src/lib.rs"), "").unwrap();
-
-  #[track_caller]
-  fn case(dir: &std::path::Path, dependency: &str, expected: Option<&str>) {
-    let output = Command::new(env!("CARGO_BIN_EXE_cargo-path"))
-      .args(["path", dependency])
-      .current_dir(dir)
-      .output()
-      .unwrap();
-
-    if let Some(expected) = expected {
-      assert!(
-        output.status.success(),
-        "{}",
-        String::from_utf8_lossy(&output.stderr),
-      );
-      assert_eq!(
-        str::from_utf8(&output.stdout).unwrap(),
-        format!("{}\n", dir.canonicalize().unwrap().join(expected).display(),),
-      );
-    } else {
-      assert!(!output.status.success());
-      assert_eq!(
-        str::from_utf8(&output.stderr).unwrap(),
-        format!("error: dependency `{dependency}` not found\n"),
-      );
-    }
-  }
-
-  case(dir.path(), "bar", Some("bar"));
-  case(dir.path(), "qux", None);
+  case(dir.path(), "regex", true);
+  case(dir.path(), "qux", false);
 }
